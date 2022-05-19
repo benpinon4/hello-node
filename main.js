41dfd986ebfc295f3b007eb386ce33b61571b759
benpinon4 <benjamin.pinon@CodeImmersives.com>
2022-05-19 14:12:18 -0400

    final submission

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("hello world")
+
+console.log("h");             console.log("h")
+console.log("HH   HH        lll lll                                  lll      dd !!!")
+console.log("HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!!")
+console.log("HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!")
+console.log("HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd     ")
+console.log("HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!! ")
+
+console.log("  _     _     _     _     _     _     _     _")
+console.log(" /     /     /     /     /     /     /     /  ")
+console.log("( c ) ( r ) ( e ) ( a ) ( t ) ( i ) ( v ) ( e )")
+console.log("  _/    _/    _/    _/    _/    _/    _/    _/ ")
